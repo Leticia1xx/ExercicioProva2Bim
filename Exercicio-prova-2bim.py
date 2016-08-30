a47d8c3d2bcf103d1d1a3afec8f524ab22456ca6
LeticiaPinn <leticiapinheiro2589@gmail.com>
2016-08-30 19:46:08 -0300

    Resposta

---
+++ Exercicio-prova-2bim.py
@@ -0,0 +1,45 @@
+Q.10.
+
+>>> n1 = eval(input("Digite um numero inteiro:"))
+Digite um numero inteiro: 5
+>>>n2 = eval(input("Digite um outro numero inteiro:"))
+Digite um outro numero inteiro: 19
+>>>print (n1,n2)
+5 19
+
+Q.11.
+
+>>> n1 = eval(input("Digite um numero inteiro:"))
+Digite um numero inteiro: 5
+>>>n2 = eval(input("Digite um outro numero inteiro:"))
+Digite um outro numero inteiro: 19
+>>>print (n1,n2)
+5 19
+>>> soma = eval(input (" A soma deles é:"))
+ A soma deles é: 24
+
+Q.12.
+
+Q.13.
+
+>>> base=eval(input("Digite um numero:"))
+Digite um numero:5
+>>> expoente=eval (input("Digite um numero:"))
+Digite um numero:3
+>>> x=1
+>>> resultado=125
+>>> while (1 <= 3):
+...resultado= 125 * 5
+>>> print("")
+>>> print ("O primeiro numero:",base)
+O primeiro numero: 5
+>>> print ("o segunto numero:",expoente)
+o segunto numero: 3
+>>> print (base,"elevado a",expoente,"é igual a:",resultado)
+5 elevado a 3 é igual a: 125
+
+
+Q.14.
+
+
+
